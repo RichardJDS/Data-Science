a142b6afc38a8437f2a19a7b312ba9ee32896b3f
RichardJDuran <duransullcamanirichard@gmail.com>
2020-09-06 16:24:57 -0400

    assigmentfinal

--- assigment3.R
+++ assigment3.R
@@ -1,4 +1,7 @@
-setwd('C:/Users/rubind1/Documents/Coursera-R')
+# inicializa inv como NULL
+# define la funcion de conjunto para un nuevo valor de la matriz en el entorno principal
+# si hay una nueva matriz, restablecer inv a NULL
+# define la función get que devuelve el valor del argumento de la matriz
 
 makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
   s <- NULL
@@ -13,7 +16,9 @@ makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
        setsolve = setsolve,
        getsolve = getsolve)
 }
-
+# Esta función calcula la inversa de la "matriz" especial devuelta por la funcion makeCacheMatrix 
+# Si ya se ha calculado la inversa
+# luego cacheSolve recuperará el inverso del caché
 cacheSolve <- function(x, ...) {
   s <- x$getsolve()
   if(!is.null(s)) {
